764a31509d2a24c555b8c0b87976d49fac34ed7a
15025V <vicogespinoza@gmail.com>
2025-11-19 21:53:18 -0600

    update-pkg

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-import  express  from "express"
-import cors from 'cors'
-import dotenv from 'dotenv'
-import authRoutes from './routes/auth'
-import citasRoutes from './routes/citas'
-import doctorRoutes from "./routes/doctorRoutes";
-dotenv.config()
+    import  express  from "express"
+    import cors from 'cors'
+    import dotenv from 'dotenv'
+    import authRoutes from './routes/auth'
+    import citasRoutes from './routes/citas'
+    import doctorRoutes from "./routes/doctorRoutes";
+    dotenv.config()
 
-const app = express()
-const PORT = process.env.PORT || 3100
+    const app = express()
+    const PORT = process.env.PORT || 3100
 
-app.use (cors())
-app.use(express.json())
+    app.use (cors())
+    app.use(express.json())
 
-app.use('/auth',authRoutes)
-app.use(doctorRoutes);
-app.use('/',citasRoutes)
+    app.use('/auth',authRoutes)
+    app.use(doctorRoutes);
+    app.use('/',citasRoutes)
 
-app.listen(PORT,()=>{
-    console.log(`Servidor escuchando en http://192.168.0.8:${PORT}`)
-})
+    app.listen(PORT,()=>{
+        console.log(`Servidor escuchando en el puerto :${PORT}`)
+    })
