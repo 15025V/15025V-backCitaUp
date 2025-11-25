16540d1aca372ab97e20d43c8fbbf4b622dd2af6
15025V <vicogespinoza@gmail.com>
2025-11-25 17:44:35 -0600

    fix: update cookie settings for dev environment

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import { login, registerDoctor } from "../controllers/authController";
 import { logout } from "../controllers/logout";
+import { verify } from "crypto";
+import { verifyToken } from "../middlewares/auth";
 
 const router = Router();
 
 router.post("/logout", logout);
 router.post("/register/doctor", registerDoctor);
 router.post("/login", login);
-
+router.get("/me",verifyToken, (req, res) => {
+    const user = (req as any).user;
+    res.json({ user });
+});
 export default router;
